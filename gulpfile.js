3838870912047a997b1f55ab7853816888b70ff9
Frank Rowe <fsrowe@gmail.com>
2015-03-11 17:00:02 -0400

    categories

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,48 @@
 /* gulpfile.js */
- 
-// Load some modules which are installed through NPM.
-var gulp = require('gulp');
-var browserify = require('browserify');  // Bundles JS.
-var reactify = require('reactify');  // Transforms React JSX to JS.
-var source = require('vinyl-source-stream');
-var del = require('del')
-var less = require('gulp-less')
-var path = require('path')
- 
-// Define some paths.
+
+var gulp = require('gulp')
+  , browserify = require('browserify')
+  , uglify = require('gulp-uglify')
+  , reactify = require('reactify')
+  , source = require('vinyl-source-stream')
+  , rename = require("gulp-rename")
+  , del = require('del')
+  , less = require('gulp-less')
+  , path = require('path')
+
 var paths = {
   css: ['./css/src/style.less'],
   app_js: ['./js/src/app.js'],
   js: ['./js/src/**/*.js'],
-};
+}
 
 gulp.task('css', function () {
   return gulp.src(paths.css)
     .pipe(less({
       paths: [ './css/src', './node_modules/bootstrap/less' ]
     }))
-    .pipe(gulp.dest('./css/'));
-});
- 
-// Our JS task. It will Browserify our code and compile React JSX files.
+    .pipe(gulp.dest('./css/'))
+})
+
 gulp.task('js', function() {
-  // Browserify/bundle the JS.
   browserify(paths.app_js)
     .transform(reactify)
     .bundle()
     .pipe(source('bundle.js'))
-    .pipe(gulp.dest('./js'));
-});
- 
-// Rerun tasks whenever a file changes.
+    .pipe(gulp.dest('./js'))
+})
+
+gulp.task('compress', function() {
+  gulp.src('./js/bundle.js')
+    .pipe(uglify())
+    .pipe(rename('./bundle.min.js'))
+    .pipe(gulp.dest('./js'))
+})
+
 gulp.task('watch', function() {
-  gulp.watch(paths.css, ['css']);
-  gulp.watch(paths.js, ['js']);
-});
- 
-// The default task (called when we run `gulp` from cli)
-gulp.task('default', ['watch', 'css', 'js']);
+  gulp.watch(paths.css, ['css'])
+  gulp.watch(paths.js, ['js'])
+})
+
+gulp.task('default', ['watch', 'css', 'js'])
+gulp.task('prod', ['css', 'js', 'compress'])
--- js/src/app.js
+++ js/src/app.js
@@ -1,24 +1,129 @@
+/* Scenic Byway Tour Guide for Lower Eastern Shore of Maryland 
+ * By Frank Rowe, 2015
+ */
+
 var React = require('react')
+  , Router = require('react-router')
+  , Route = Router.Route
+  , NotFoundRoute = Router.NotFoundRoute
+  , DefaultRoute = Router.DefaultRoute
+  , Link = Router.Link
+  , RouteHandler = Router.RouteHandler
+  , _ = require('lodash')
+
+React.initializeTouchEvents(true)
 
 var key = '1AS60Zm5ytMoOI3AhvR0dcOF-G_3j2lpZR7VRBAzfBgE'
 
 var sheet = Tabletop.init({
   key: key,
-  callback: handleSheet,
-  simpleSheet: true,
-  debug: true
+  callback: init,
+  simpleSheet: true
 })
 
-function handleSheet(data, tabletop) {
-  React.render(<App data={data} />, document.getElementById('app'))
+function init(data, tabletop) {
+  Router.run(routes, function (Handler) {
+    React.render(<Handler data={data}/>, document.getElementById('app'))
+  })
 }
- 
-var App = React.createClass({
+
+var Header = React.createClass({
   render: function() {
-    var rows = this.props.data.map(function(row) {
-      return <li><img src={row.image}/> {row.name}</li>
-    })
-    return <ul>{rows}</ul>
+    return (
+      <div className="header">
+        <h5><span>Maryland Lower Eastern Shore</span></h5>
+        <h1><span>Scenic Byway</span></h1>
+        <h3><span>Mobile Tour Guide</span></h3>
+      </div>
+    )
   }
 })
 
+var Site = React.createClass({
+  mixins: [Router.State],
+  render: function() {
+    var id = +this.getParams().siteID
+    var site =  this.props.data[id]
+    return (
+      <div className="site-details page">
+        <div className="page-title">
+          <Link to="categoryName" params={{categoryName: site.category}}><span className="home-button"><i className="fa fa-arrow-left"></i></span></Link>
+          <h3>Site Details</h3>
+        </div>
+        <div className="page-inner">
+          <p>Site Name: {site.name}</p>
+          <img className="img-responsive" src={site.image} />
+        </div>
+      </div>
+    )
+  }
+})
+
+var Category = React.createClass({
+  mixins: [Router.State],
+  render: function() {
+    var self = this
+    var rows = this.props.data.map(function(site, idx) {
+      if (site.category === self.getParams().categoryName) {
+        var p = {siteID: idx}
+        return <Link to="siteID" params={p} key={idx}><li className="list-group-item">{site.name}</li></Link>
+      }
+    })
+    rows = _.compact(rows)
+    return (
+      <div className="category page">
+        <div className="page-title">
+          <Link to="/"><span className="home-button"><i className="fa fa-home"></i></span></Link>
+          <h3>{this.getParams().categoryName}</h3>
+        </div>
+        <div className="page-inner">
+          <ul className="list-group">{rows}</ul>
+        </div>
+      </div>
+    )
+  }
+})
+
+var CategoryList = React.createClass({
+  render: function() {
+    var rows = this.props.categories.map(function(category) {
+      var p = {categoryName: category}
+      return <Link to="categoryName" params={p} key={category}><li className="list-group-item">{category}</li></Link>
+    })
+    return <div className="category-list"><ul className="list-group">{rows}</ul></div>
+  }
+})
+
+var Home = React.createClass({
+  render: function() {
+    var categories = _.uniq(_.pluck(this.props.data, 'category'))
+    return (
+      <div className="home page">
+        <Header />
+        <CategoryList categories={categories} />
+      </div>
+    )
+  }
+})
+
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <RouteHandler data={this.props.data}/>
+    )
+  }
+})
+
+var routes = (
+  <Route name="app" path="/" handler={App}>
+    <Route name="home" handler={Home}/>
+    <Route name="category" handler={Category}>
+      <Route name="categoryName"  path=":categoryName" handler={Category}/>
+    </Route>
+    <Route name="site" handler={Site}>
+      <Route name="siteID"  path=":siteID" handler={Category}/>
+    </Route>
+    <DefaultRoute handler={Home}/>
+  </Route>
+)
